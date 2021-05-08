f22d5ac375f5d588bd0d017e6b0d7c8b2b078f76
d-capital <dzyuba.stanislaw@mail.ru>
2021-05-08 10:35:21 +0300

    new

---
+++ src/app/portfolios/portfolios-api.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable, of, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import {API_URL} from '../env';
+import {Portfolio} from './portfolio.model';
+
+
+
+@Injectable()
+export class PortfoliosApiService {
+private portfoliosUrl = `${API_URL}/manage_portfolios`;
+  constructor(private http: HttpClient) {
+    
+  }
+
+  // GET list of public, future events
+  getPortfolios(): Observable<Portfolio[]> {
+    return this.http.get<Portfolio[]>(this.portfoliosUrl).pipe(catchError(this.erroHandler));
+  }
+  erroHandler(error: HttpErrorResponse) {
+    return throwError(error.message || 'server Error');
+  }
+}
