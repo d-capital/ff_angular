6e2b14572a73313f3b0ee04743f6bae92b2c89d5
d-capital <dzyuba.stanislaw@mail.ru>
2022-11-21 19:04:26 +0100

    new url

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,4 +1,5 @@
-export const API_URL = 'http://127.0.0.1:5000/';
+export const API_URL = 'https://finefolioapi.onrender.com/';
 //'http://127.0.0.1:5000/' - local
 //'https://finefolio.azurewebsites.net/'
 //'https://finefolio.herokuapp.com/'
+//'https://finefolioapi.onrender.com/'
