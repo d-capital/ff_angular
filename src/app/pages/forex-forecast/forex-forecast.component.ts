70105a21c1b0449804b682ea4a3775edf1d47d98
sdziuba <sdziuba@outlook.com>
2025-03-21 09:22:54 +0100

    fair rates

--- src/app/pages/forex-forecast/forex-forecast.component.ts
+++ src/app/pages/forex-forecast/forex-forecast.component.ts
@@ -2,6 +2,7 @@ import { NgIf } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ForexForecastService } from 'src/app/services/forex-forecast.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-forex-test',
@@ -11,12 +12,18 @@ export class ForexForecastComponent implements OnInit {
 
     constructor(
         private forexForecastService: ForexForecastService,
+        private router: Router,
     ) { }
+
     serverErrors = [];
     public currentExchangeRate: number = 0.0;
     public futureExchangeRate: number = 0.0;
     public regressionExchangeRate: number = 0.0;
+    public pppRate: number = 0.0;
+    public irpRate: number = 0.0;
+    public fairExchangeRate: number = 0.0;
     public selectedCurrencyPair: string = "";
+    currencyPairs: string[] = ['EUR/USD', 'USD/JPY', 'NZD/JPY','CAD/JPY','GBP/USD', 'CHF/JPY','EUR/NZD','USD/CHF','EUR/CAD','USDCAD','AUD/USD'];
 
     ngOnInit(): void {
         this.getForecast();
@@ -32,6 +39,9 @@ export class ForexForecastComponent implements OnInit {
             this.currentExchangeRate = data['exchange_rate'];
             this.futureExchangeRate = data['future_exchange_rate'];
             this.regressionExchangeRate = data['regression_forecast'];
+            this.pppRate = data['fair_values']['ppp_rate'];
+            this.irpRate = data['fair_values']['irp_rate'];
+            this.fairExchangeRate = data['fair_values']['fair_exchange_rate'];
         }, err => {
             const validationErrors = err.error;
             if (err instanceof HttpErrorResponse) {
@@ -40,5 +50,13 @@ export class ForexForecastComponent implements OnInit {
                 }
             }
         });
+    }
+    loadCurrencyPair(value):void
+    {
+       this.selectedCurrencyPair = value;
+       localStorage.setItem("currencyPair", value)
+       this.getForecast();
+       window.location.reload();
+       //this.router.navigate(["BarometerComponent"]);
     }
 }
